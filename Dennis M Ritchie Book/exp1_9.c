135236dfd7990b70c2f5caef96f1e792d87d474c
Nikhil L N Pandarge <33156781+Nikhil-Pandarge@users.noreply.github.com>
2020-10-20 09:58:14 +0530

    C Programs By Nikhil
    Daily Update 1.1

--- Dennis M Ritchie Book/exp1_9.c
+++ Dennis M Ritchie Book/exp1_9.c
@@ -1,12 +1,1 @@
-#include<stdio.h>
 
-void main()
-{
-    char c,last_c;
-    while((c = getchar()) != EOF)
-    {
-        if(c !=' ' || last_c != ' ')
-            putchar(c);
-        last_c = c;
-    }
-}
